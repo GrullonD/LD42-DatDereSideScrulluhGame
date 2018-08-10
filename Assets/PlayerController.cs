e7b013b41ca67492df42adc60f11c6271c10efe0
David Grullon <DGrullon@gmail.com>
2018-08-10 18:55:23 -0400

    FirstCommt

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] int playerSpeed = 10;
+    [SerializeField] int playerJumpPower = 1250;
+
+    private bool facingRight = true;
+    private float moveX;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        // Controls
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+        // Animations
+
+        // Player Direction
+        if (moveX < 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+
+        // Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    private void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    private void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+}
